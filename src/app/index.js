ecc5652ad0f318111c1169958173dca0e6897738
Nodd <124269320+Indio01@users.noreply.github.com>
2024-09-03 12:24:36 +0000

    [feat] signIn

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useAuth } from "../hooks/auth";
 
 export default function App() {
+  const { signIn, signOut } = useAuth();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Aplicativo Pronto para Usar</Text>
+      <Button
+        title="Signin Super"
+        onPress={() =>
+          signIn({ email: "super@gmail.com", password: "Super123!" })
+        }
+      />
+      <Button
+        title="Signin Adm"
+        onPress={() =>
+          signIn({ email: "adm@gmail.com", password: "Adm123!" })
+        }
+      />
+      <Button
+        title="Signin User"
+        onPress={() =>
+          signIn({ email: "user@gmail.com", password: "User123!" })
+        }
+      />
+      <Button title="Signout" onPress={() => signOut()} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,12 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title:{
+  title: {
     fontFamily: "regular",
     fontSize: 20,
-  }
+  },
 });
