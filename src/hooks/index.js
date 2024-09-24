fbd18314d41c2f61193db6a176b59dd82983dc42
Nodd <124269320+Indio01@users.noreply.github.com>
2024-09-24 12:51:13 +0000

    [feat]Aula06-20:40

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,10 +1,14 @@
 import { AuthProvider } from "./auth";
+import { DataProvider } from "./data";
 import { FontProvider } from "./font";
 
-export function AppProvider({children}){
-    return (
+export function AppProvider({ children }) {
+  return (
     <FontProvider>
+      <DataProvider>
+        
         <AuthProvider>{children}</AuthProvider>
+      </DataProvider>
     </FontProvider>
-    );
+  );
 }
